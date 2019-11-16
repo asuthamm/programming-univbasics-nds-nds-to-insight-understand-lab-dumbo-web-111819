d465368a4f9b0a274c1d7b2f8b28ac71102c501d
Alex Suthammanont <asuthamm@gmail.com>
2019-11-16 02:52:18 +0000

    Automatically backed up by Learn

---
+++ lib/test.rb
@@ -0,0 +1,22 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'directors_database'
+require 'pp'
+# Call the method directors_database to retrieve the NDS
+
+def pretty_print_nds(nds)
+  # Change the code below to pretty print the nds with pp
+  pp nds
+end
+
+def print_first_directors_movie_titles
+  movie = directors_database
+  movie[0][:movies].each.with_index do |ele,idx|
+    puts movie[0][:movies][idx][:title]
+  end
+end
+
+# pretty_print_nds(directors_database)
+# print_first_directors_movie_titles
+
+# p directors_database[0][:name]
+p directors_database[0][:movies][0]
